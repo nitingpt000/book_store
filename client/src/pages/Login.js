760cbe0daf355b7e18a6ca765bec0f97c4b26d32
nitingpt <nitin.g@rejolut.com>
2023-03-02 03:27:46 +0530

    [feat] - add pages

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthenticationLayout from "../components/AuthenticationLayout";
+import LoginForm from "../components/LoginForm";
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import Login from "./Login";
+import BookDetail from "./BookDetail";
+import Books from "./Books";
+import Checkout from "./Checkout";
+import Orders from "./Orders";
+
+export { Home, Login, BookDetail, Books, Checkout, Orders };
