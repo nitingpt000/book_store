46464ae5700dc96cd467d7aaa9035f93ef9e39bd
blocknitin <nitin_gupta@blockapps.net>
2023-03-02 00:06:15 +0530

    [feat] - add book routes

--- server/api/v1/routes.js
+++ server/api/v1/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import order from "./Order";
 import user from "./User";
-import { Order, User } from "./endpoints";
+import book from "./Book";
+import { Order, User,Book } from "./endpoints";
 const router = express.Router();
 
 router.use(Order.prefix, order);
 router.use(User.prefix, user);
+router.use(Book.prefix,book);
 
 export default router;
